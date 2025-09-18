a119942a2c711ccfc48e333efb7a1860adc1b167
Laurence-LaurencE <laurence20gros@gmail.com>
2025-09-18 21:04:51 +0200

    datas.. modif routers:routes paiement ds BookingRooteur

--- routes/paymentRouter.ts
+++ routes/paymentRouter.ts
@@ -1,13 +1,13 @@
-import { Router } from "express";
+import { response, Router } from "express";
 import { PaymentController } from "../controllers/PaymentController";
+import { request } from "node:http";
 
 //
 const paymentRouter = Router();
 
 //paymentform
 
-//test
-
+//pas besoin de get pour lire : 
 // paymentRouter.get("/", (request, response) => {
 //   const controller = new PaymentController(request, response);
 //   controller.paymentpage();
@@ -25,6 +25,15 @@ paymentRouter.post("/traitement", (request, response) => {
   controller.addPayment();
 });
 
-//put 
+//put
+
+//confirmation
+
+
+//get /:id
+paymentRouter.get("/confirmation",(request,response) => {
+  const controller = new PaymentController(request, response);
+  controller.confirmation();
+})
 
 export default paymentRouter;
